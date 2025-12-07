43d80565d80dfebc2263c42d13c8fd198c08e2ce
ALI RAZA <razadeveloper816@gmail.com>
2025-12-06 19:35:22 -0800

    complete topsection styling

--- src/Component/ProductDetail/TopSection.jsx
+++ src/Component/ProductDetail/TopSection.jsx
@@ -1,49 +1,43 @@
 import { IoStarSharp } from "react-icons/io5";
-
+import style from '../ProductDetail/TopSection.module.css';
 
 export default function TopSection() {
     return (
         <div>
-            <div className="imageGrid">
-                <div className="left">
-                    <div className="relatedimages">
+            <div className={style.imageGrid}>
+                <div className={style.left}>
+                    <div className={style.relatedimages}>
+                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
+                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
+                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
                         <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
                     </div>
-                    <div className="relatedimages">
-                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
-                    </div>
-                    <div className="relatedimages">
-                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
-                    </div>
-                    <div className="relatedimages">
-                        <div><img src="/Images/collections/Rectangle 3608.png" alt="" /></div>
-                    </div>
-                    <div className="productImage">
+                    <div className={style.productImage}>
                         <img src="/Images/collections/Rectangle 3608.png" alt="" />
                     </div>
                 </div>
-                <div className="right">
+                <div className={style.right}>
                     <h1>Men Round Neck Pure Cotton T-Shirt</h1>
                     <div className="ratings">
-                        <span><IoStarSharp /></span>
-                        <span><IoStarSharp /></span>
-                        <span><IoStarSharp /></span>
-                        <span><IoStarSharp /></span>
-                        <span><IoStarSharp /></span>
-                        <span>(122)</span>
+                        <span><IoStarSharp style={{marginRight:'.5rem',color:'#FF532E',fontSize:''}} /></span>
+                        <span><IoStarSharp style={{marginRight:'.5rem',color:'#FF532E',fontSize:''}} /></span>
+                        <span><IoStarSharp style={{marginRight:'.5rem',color:'#FF532E',fontSize:''}} /></span>
+                        <span><IoStarSharp style={{marginRight:'.5rem',color:'#FF532E',fontSize:''}} /></span>
+                        <span><IoStarSharp style={{marginRight:'.5rem',color:'#FF532E',fontSize:''}} /></span>
+                        <span style={{color:'#1C1C1C',fontSize:'16px'}}>(122)</span>
                     </div>
-                    <div>
+                    <div className={style.price}>
                         <h1>$149</h1>
                         <p>A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.</p>
                     </div>
-                    <div>
+                    <div className={style.size}>
                         <h3>Select Size</h3>
-                        <div className="size">
-                            <input type="text" value="S" />
-                            <input type="text" value="M" />
-                            <input type="text" value="L" />
-                            <input type="text" value="XL" />
-                            <input type="text" value="XXL" />
+                        <div className={style.sizesNumber}>
+                            <span>S</span>
+                            <span>M</span>
+                            <span>X</span>
+                            <span>XL</span>
+                            <span>XXL</span>
                         </div>
                         <button>Add to cart</button>
                     </div>
