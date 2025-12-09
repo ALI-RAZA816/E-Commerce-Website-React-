277aae3891145acc7ac7575a50477cabfd41bbf6
ALI RAZA <razadeveloper816@gmail.com>
2025-12-09 12:07:13 -0800

    add data

--- productData.js
+++ productData.js
@@ -1,52 +1,310 @@
 export const collectionProduct = [
-        {
-            img:'/Images/collections/Rectangle 3608.png',
-            title:'Women Round Neck Cotton Top',
-            price:'149'
-        },
-        {
-            img:'/Images/collections/Rectangle 3609.png',
-            title:'Women Round Neck Cotton Top',
-            price:'40'
-        },
-        {
-            img:'/Images/collections/Rectangle 3619.png',
-            title:'Women Round Neck Cotton Top',
-            price:'40'
-        },
-        {
-            img:'/Images/collections/Rectangle 3611.png',
-            title:'Men Round Neck Pure Cotton T-shirt',
-            price:'46'
-        },
-        {
-            img:'/Images/collections/Rectangle 3635.png',
-            title:'Men Round Neck Pure Cotton T-shirt',
-            price:'26'
-        },
-        {
-            img:'/Images/collections/Rectangle 3613.png',
-            title:'Men Printed Plain Cotton Shirt',
-            price:'52'
-        },
-        {
-            img:'/Images/collections/Rectangle 3615.png',
-            title:'Women Round Neck Cotton Top',
-            price:'30'
-        },
-        {
-            img:'/Images/collections/Rectangle 3616.png',
-            title:'Women Round Neck Cotton Top',
-            price:'34'
-        },
-         {
-            img:'/Images/collections/Rectangle 3634.png',
-            title:'Men Round Neck Pure Cotton T-shirt',
-            price:'26'
-        },
-         {
-            img:'/Images/collections/Rectangle 3617.png',
-            title:'Men Round Neck Pure Cotton T-shirt',
-            price:'68'
-        },
-    ]
+    {
+        img: '/Images/collections/Rectangle 3609.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '40',
+        category:'Women',
+        type:"Topwear"
+    },
+    {
+        img: '/Images/collections/Rectangle 3619.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '40',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img3.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '35',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3635.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '26',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3615.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '30',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img6.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '55',
+        category:'Women',
+        type:'Topwear',
+    },
+    {
+        img: '/Images/collections/p_img7.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '25',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3617.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '68',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img9.png',
+        title: 'Girls Round Neck Pure Cotton T-shirt',
+        price: '12',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img10.png',
+        title: 'Men Bottomwear Trousers',
+        price: '45',
+        category:'Men',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3611.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '46',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3634.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '26',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3608.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '149',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img14.png',
+        title: 'Boy Round Neck Pure Cotton T-shirt',
+        price: '66',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img15.png',
+        title: 'Men Bottomwear Trousers',
+        price: '95',
+        category:'Men',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img16.png',
+        title: 'Girls Round Neck Pure Cotton T-shirt',
+        price: '13',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img17.png',
+        title: 'Men Topered Fit Flat-Front Trousers',
+        price: '32',
+        category:'Men',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img18.png',
+        title: 'Boy Round Neck Pure Cotton T-shirt',
+        price: '22',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img19.png',
+        title: 'Boy Round Neck Pure Cotton T-shirt',
+        price: '66',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img20.png',
+        title: 'Women Palazzo Pants with Waist Belt',
+        price: '76',
+        category:'Women',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img21.png',
+        title: 'Women Zip-Front Relaxed Fit Jacket',
+        price: '56',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img22.png',
+        title: 'Women Palazzo Pants with Waist Belt',
+        price: '92',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img23.png',
+        title: 'Boy Round Neck Pure Cotton T-shirt',
+        price: '54',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img24.png',
+        title: 'Boy Round Neck Pure Cotton T-shirt',
+        price: '88',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img25.png',
+        title: 'Girls Round Neck Pure Cotton T-shirt',
+        price: '33',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img26.png',
+        title: 'Women Zip-Front Relaxed Fit Jacket',
+        price: '11',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img27.png',
+        title: 'Girls Round Neck Pure Cotton T-shirt',
+        price: '201',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img28.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '40',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3616.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '34',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img34.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '59',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img35.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '52',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img36.png',
+        title: 'Women Round Neck Cotton Top',
+        price: '22',
+        category:'Women',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/Rectangle 3613.png',
+        title: 'Men Printed Plain Cotton Shirt',
+        price: '52',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img40.png',
+        title: 'Men Slim Fit Relaxed Denim Jacket',
+        price: '19',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img41.png',
+        title: 'Men Round Neck Pure Cotton T-shirt',
+        price: '16',
+        category:'Men',
+        type:'Topwear'
+    },
+    {
+        img: '/Images/collections/p_img43.png',
+        title: 'Kid Tapered Slim Fit Trouser',
+        price: '26',
+        category:'Kids',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img44.png',
+        title: 'Women Zip-Front Relaxed Fit Jacket',
+        price: '69',
+        category:'Women',
+        type:'Winerwear'
+    },
+    {
+        img: '/Images/collections/p_img46.png',
+        title: 'Men Slim Fit Relaxed Denim Jacket',
+        price: '59',
+        category:'Men',
+        type:'Winterwear'
+    },
+    {
+        img: '/Images/collections/p_img47.png',
+        title: 'Kid Tapered Slim Fit Trouser',
+        price: '71',
+        category:'Kids',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img48.png',
+        title: 'Men Slim Fit Relaxed Denim Jacket',
+        price: '11',
+        category:'Men',
+        type:'Winterwear'
+    },
+    {
+        img: '/Images/collections/p_img49.png',
+        title: 'Kid Tapered Slim Fit Trouser',
+        price: '15',
+        category:'Kids',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img50.png',
+        title: 'Kid Tapered Slim Fit Trouser',
+        price: '23',
+        category:'Kids',
+        type:'Bottomwear'
+    },
+    {
+        img: '/Images/collections/p_img51.png',
+        title: 'Women Zip-Front Relaxed Fit Jacket',
+        price: '10',
+        category:'Women',
+        type:'Winterwear'
+    },
+    {
+        img: '/Images/collections/p_img52.png',
+        title: 'Men Slim Fit Relaxed Denim Jacket',
+        price: '15',
+        category:'Men',
+        type:'Winterwear'
+    },
+]
