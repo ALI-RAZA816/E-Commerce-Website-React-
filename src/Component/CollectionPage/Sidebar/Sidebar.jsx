57bbdb32310118691e0e1bf0eab3c2ff823194bb
ALI RAZA <razadeveloper816@gmail.com>
2025-12-10 23:06:11 -0800

    add dynamic filtering inputs

--- src/Component/CollectionPage/Sidebar/Sidebar.jsx
+++ src/Component/CollectionPage/Sidebar/Sidebar.jsx
@@ -1,39 +1,45 @@
+import { useSelector } from 'react-redux';
 import style from '../Sidebar/Sidebar.module.css';
 import { BsFillFilterSquareFill } from "react-icons/bs";
 
-export default function Sidebar({showFilterBox, showFilter}) {
+export default function Sidebar({ showFilterBox, showFilter }) {
+
+  const products = useSelector((store) => store.homeProducts);
+  const category = [];
+  const type = [];
+  products.map(item => {
+    if (!category.includes(item.category)) {
+      category.push(item.category);
+      return;
+    }
+  });
+  products.map(item => {
+    if (!type.includes(item.type)) {
+      type.push(item.type);
+      return;
+    }
+  });
+  console.log(type);
   return (
     <div className={`${style.sidebar} ${showFilter === true && style.active}`}>
-      <h1><BsFillFilterSquareFill onClick={showFilterBox} className={style.filterIcon}/>Filters</h1>
-      <div className={style.category} style={{marginBottom:'1rem'}}>
+      <h1><BsFillFilterSquareFill onClick={showFilterBox} className={style.filterIcon} />Filters</h1>
+      <div className={style.category} style={{ marginBottom: '1rem' }}>
         <h3>Categories</h3>
-        <div>
-            <input type="checkbox" id='men' />
-            <label htmlFor="men">Men</label>
-        </div>
-        <div>
-            <input type="checkbox" id='women' />
-            <label htmlFor="women">Women</label>
-        </div>
-        <div>
-            <input type="checkbox" id='kids' />
-            <label htmlFor="kids">Kids</label>
-        </div>
+        {category.map(item => {
+          return <div>
+            <input type="checkbox" id= {item} />
+            <label htmlFor={item} >{item}</label>
+          </div>
+        })}
       </div>
       <div className={style.type}>
         <h3>Type</h3>
-        <div>
-            <input type="checkbox" id='topwear' />
-            <label htmlFor="topwear">Topwear</label>
-        </div>
-        <div>
-            <input type="checkbox" id='bottomwear' />
-            <label htmlFor="bottomwear">Bottomwear</label>
-        </div>
-        <div>
-            <input type="checkbox" id='winterwear' />
-            <label htmlFor="winterwear">Winterwear</label>
-        </div>
+        {type.map(item => {
+          return <div>
+            <input type="checkbox" id={item} />
+            <label htmlFor={item}>{item}</label>
+          </div>
+        })}
       </div>
     </div>
   )
